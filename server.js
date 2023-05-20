809bb0f06d367cf88960cf4827a23d3572b71c49
Givannixs <giovanni.oliveiasilva@gmail.com>
2023-05-20 12:18:51 -0300

    Revert "Função Cliente"
    This reverts commit 883c3aa29258ba0dc399b5b4e080094d83ff366e.

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const expressLayouts = require('express-ejs-layouts');
 const HomeRoute = require('./routes/homeRoute');
 const ProdutoRoute = require('./routes/produtoRoute');
 const CategoriaRoute = require('./routes/categoriaRoute');
-const ClienteRoute = require ('./routes/clienteRoute');
 
 const app = express();
 
@@ -29,8 +28,6 @@ let produtoRota = new ProdutoRoute();
 app.use('/produto', produtoRota.router);
 let categoriaRota = new CategoriaRoute();
 app.use('/categoria', categoriaRota.router);
-let clienteRota = new ClienteRoute();
-app.use('/cliente', clienteRota.router);
 
 
 //ponto de inicio do nosso servidor web
